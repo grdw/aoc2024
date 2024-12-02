0f06aee3937244520230c907f80adb6126ab3ac4
grdw <gerard@grdw.nl>
2024-12-02 11:24:15 +0100

    refactor a bit

--- p01/src/main.rs
+++ p01/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 fn main() {
     let (mut l, mut m) = parse("input");
+
     println!("p1 {}", total_distance(&mut l, &mut m));
     println!("p2 {}", similarity_score(&l, &m));
 }
@@ -26,34 +27,33 @@ fn total_distance(l: &mut Vec<u32>, m: &mut Vec<u32>) -> u32 {
     l.sort();
     m.sort();
 
-    let mut d = 0;
-    for i in 0..l.len() {
-        d += l[i].abs_diff(m[i])
-    }
-    return d
+    (0..l.len())
+        .map(|i| l[i].abs_diff(m[i]))
+        .sum()
 }
 
 #[test]
 fn test_distance() {
     let (mut v1, mut v2) = parse("test_input");
+
     assert_eq!(total_distance(&mut v1, &mut v2), 11)
 }
 
-fn similarity_score(l: &Vec<u32>, m: &Vec<u32>) -> u32 {
+fn similarity_score(list: &Vec<u32>, m: &Vec<u32>) -> u32 {
     let mut h: HashMap<u32, u32> = HashMap::new();
-    let mut s = 0;
     for i in m.iter() {
         h.entry(*i).and_modify(|c| *c += 1).or_insert(1);
     }
 
-    for j in l.iter() {
-        s += j * h.get(j).unwrap_or(&0)
-    }
-    s
+    list
+        .iter()
+        .map(|j| j * h.get(j).unwrap_or(&0))
+        .sum()
 }
 
 #[test]
 fn test_similarity_score() {
     let (v1, v2) = parse("test_input");
+
     assert_eq!(similarity_score(&v1, &v2), 31)
 }
