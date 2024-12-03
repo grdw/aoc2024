3fce5532e3dccf4ce1cd67f2f473fd8a21b86cf8
Gerard <gerard@grdw.nl>
2024-12-03 11:53:54 +0100

    Clean up p03

--- p03/src/main.rs
+++ p03/src/main.rs
@@ -1,13 +1,16 @@
 use std::fs;
-use std::io;
+use std::ops::Range;
 use regex::Regex;
 
 struct Instruction(u32, u32);
 
 fn main() {
-    let instructions = parse("input").unwrap();
+    let shopkeeper_data = fs::read_to_string("input").unwrap();
+    let skips = parse_skips(&shopkeeper_data);
+    let instructions = parse(&shopkeeper_data, &vec![]);
     println!("p1 {}", multiply(&instructions));
-    let instructions = parse_with_skips("input").unwrap();
+
+    let instructions = parse(&shopkeeper_data, &skips);
     println!("p2 {}", multiply(&instructions));
 }
 
@@ -15,56 +18,51 @@ fn multiply(instructions: &Vec<Instruction>) -> u32 {
     instructions.iter().map(|i| i.0 * i.1).sum()
 }
 
-fn parse(input: &'static str) -> io::Result<Vec<Instruction>> {
-    let shopkeeper_data = fs::read_to_string(input)?;
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-
-    Ok(
-        re
-            .captures_iter(&shopkeeper_data)
-            .map(|caps| {
-                let (_, [ls, rs]) = caps.extract();
-                let left = ls.parse::<u32>().unwrap();
-                let right = rs.parse::<u32>().unwrap();
-
-                Instruction(left, right)
-            })
-            .collect()
-    )
-}
-
 #[test]
 fn test_mul_input() {
-    let t = parse("test_input").unwrap();
+    let shopkeeper_data = fs::read_to_string("test_input").unwrap();
+    let t = parse(&shopkeeper_data, &vec![]);
+
     assert_eq!(multiply(&t), 161)
 }
 
-fn parse_with_skips(input: &'static str) -> io::Result<Vec<Instruction>> {
-    let shopkeeper_data = fs::read_to_string(input)?;
-    let re = Regex::new(r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))").unwrap();
-    let mut enabled = true;
-    let mut instructions = vec![];
+fn parse_skips(input: &String) -> Vec<Range<usize>> {
+    let skips_re = Regex::new(r"don't\(\).*do\(\)").unwrap();
+    skips_re
+        .captures_iter(&input)
+        .map(|caps| {
+            let m = caps.get(0).unwrap();
+            m.start()..m.end()
+        })
+        .collect()
+}
 
-    for caps in re.captures_iter(&shopkeeper_data) {
-        let s = caps.get(1);
-        let l = caps.get(2);
-        let r = caps.get(3);
+fn parse(input: &String, skips: &Vec<Range<usize>>) -> Vec<Instruction> {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
-        if l.is_none() {
-            let signal = s.unwrap().as_str();
-            enabled = signal == "do()";
-        } else if enabled {
-            let left = l.unwrap().as_str().parse::<u32>().unwrap();
-            let right = r.unwrap().as_str().parse::<u32>().unwrap();
-            instructions.push(Instruction(left, right));
-        }
-    }
+    re
+        .captures_iter(&input)
+        .filter(|caps| {
+            let m = caps.get(0).unwrap();
+            let n = m.start()..m.end();
 
-    Ok(instructions)
+            !skips.iter().any(|r| r.start <= n.end && n.start <= r.end)
+        })
+        .map(|caps| {
+            let (_, [ls, rs]) = caps.extract();
+            let left = ls.parse::<u32>().unwrap();
+            let right = rs.parse::<u32>().unwrap();
+
+            Instruction(left, right)
+        })
+        .collect()
 }
 
 #[test]
 fn test_mul_input_with_skips() {
-    let t = parse_with_skips("test_input2").unwrap();
+    let shopkeeper_data = fs::read_to_string("test_input2").unwrap();
+    let skips = parse_skips(&shopkeeper_data);
+    let t = parse(&shopkeeper_data, &skips);
+
     assert_eq!(multiply(&t), 48)
 }
