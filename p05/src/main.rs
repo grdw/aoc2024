064eedaca904de5d3a5f87d983cdcc103b3c78ff
grdw <gerard@grdw.nl>
2024-12-05 10:18:48 +0100

    resolve part 2

--- p05/src/main.rs
+++ p05/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::cmp::Ordering;
 
 type OrderRules = (u32, u32);
 type Pages = Vec<u32>;
@@ -7,6 +8,7 @@ fn main() {
     let (ordered_rules, pages) = parse("input");
 
     println!("p1 {}", ordered_pages(&ordered_rules, &pages));
+    println!("p2 {}", unordered_pages(&ordered_rules, &pages));
 }
 
 fn parse(input: &'static str) -> (Vec<OrderRules>, Vec<Pages>) {
@@ -63,4 +65,53 @@ fn test_count_ordered_pages() {
     let (ordered_rules, pages) = parse("1");
 
     assert_eq!(ordered_pages(&ordered_rules, &pages), 143);
+}
+
+fn unordered_pages(rules: &Vec<OrderRules>, book: &Vec<Pages>) -> u32 {
+    let mut unordered_pages = vec![];
+
+    for pages in book {
+        let in_order = rules.iter().all(|rule| {
+            let lf = pages.iter().position(|&p| p == rule.0);
+            let rf = pages.iter().position(|&p| p == rule.1);
+
+            match (lf, rf) {
+                (Some(l), Some(r)) => r > l,
+                _ => true
+            }
+        });
+
+        if !in_order {
+            unordered_pages.push(pages);
+        }
+    }
+
+
+    let mut total = 0;
+    for u in unordered_pages.into_iter() {
+        let mut t = u.clone();
+
+        t.sort_by(|a, b| {
+            let lr = rules
+                .iter()
+                .find(|&&(l, r)| l == *a && r == *b);
+
+            if lr.is_some() {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        });
+
+        total += t[t.len() / 2];
+    }
+
+    total
+}
+
+#[test]
+fn test_count_unordered_pages() {
+    let (ordered_rules, pages) = parse("1");
+
+    assert_eq!(unordered_pages(&ordered_rules, &pages), 123);
 }
