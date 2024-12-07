2885b4909eb064acef7600d8371eae8bb244cbda
grdw <gerard@grdw.nl>
2024-12-07 09:17:45 +0100

    Refactor p07

--- p07/src/main.rs
+++ p07/src/main.rs
@@ -6,32 +6,32 @@ type TestValues = HashMap<u64, Vec<u64>>;
 fn main() {
     let test_values = parse("input");
 
-    println!("p1 {}", correct_test_values(&test_values));
-    println!("p2 {}", concat_correct(&test_values));
+    println!("p1 {}", correct_test_values(&test_values, false));
+    println!("p2 {}", correct_test_values(&test_values, true));
 }
 
 fn parse(input: &'static str) -> TestValues {
     let mut map = HashMap::new();
 
     for line in fs::read_to_string(input).unwrap().lines() {
-        let (total_s, numbers_s) = line.split_once(": ").unwrap();
+        let (total_s, nums_s) = line.split_once(": ").unwrap();
         let total = total_s.parse::<u64>().unwrap();
-        let numbers = numbers_s
+        let nums = nums_s
             .split(" ")
             .map(|i| i.parse::<u64>().unwrap())
             .collect();
 
-        map.insert(total, numbers);
+        map.insert(total, nums);
     }
 
     map
 }
 
-fn correct_test_values(test_values: &TestValues) -> u64 {
+fn correct_test_values(test_values: &TestValues, conc: bool) -> u64 {
     let mut t = 0;
 
-    for (total, numbers) in test_values {
-        if is_corr(numbers[0], *total, 1, numbers) {
+    for (total, nums) in test_values {
+        if is_corr(nums[0], *total, 1, nums, conc) {
             t += total
         }
     }
@@ -39,46 +39,27 @@ fn correct_test_values(test_values: &TestValues) -> u64 {
     t
 }
 
-fn is_corr(it: u64, total: u64, i: usize, numbers: &Vec<u64>) -> bool {
-    if i == numbers.len() {
+fn is_corr(
+    it: u64,
+    total: u64,
+    i: usize,
+    nums: &Vec<u64>,
+    conc: bool) -> bool {
+
+    if i == nums.len() {
         return it == total
     }
 
-    is_corr(it + numbers[i], total, i + 1, numbers) ||
-    is_corr(it * numbers[i], total, i + 1, numbers)
+    is_corr(it + nums[i], total, i + 1, nums, conc) ||
+    is_corr(it * nums[i], total, i + 1, nums, conc) ||
+    (conc && is_corr(concat(it, nums[i]), total, i + 1, nums, conc))
 }
 
 #[test]
 fn test_correct_test_values() {
     let test_values = parse("1");
-    assert_eq!(correct_test_values(&test_values), 3749)
-}
 
-fn concat_correct(test_values: &TestValues) -> u64 {
-    let mut t = 0;
-
-    for (total, numbers) in test_values {
-        if is_corr_concat(numbers[0], *total, 1, numbers) {
-            t += total
-        }
-    }
-
-    t
-}
-
-fn is_corr_concat(
-    it: u64,
-    total: u64,
-    i: usize,
-    numbers: &Vec<u64>) -> bool {
-
-    if i == numbers.len() {
-        return it == total
-    }
-
-    is_corr_concat(it + numbers[i], total, i + 1, numbers) ||
-    is_corr_concat(it * numbers[i], total, i + 1, numbers) ||
-    is_corr_concat(concat(it, numbers[i]), total, i + 1, numbers)
+    assert_eq!(correct_test_values(&test_values, false), 3749)
 }
 
 fn concat(n: u64, m: u64) -> u64 {
@@ -96,5 +77,6 @@ fn test_concat() {
 #[test]
 fn test_concat_correct() {
     let test_values = parse("1");
-    assert_eq!(concat_correct(&test_values), 11387)
+
+    assert_eq!(correct_test_values(&test_values, true), 11387)
 }
