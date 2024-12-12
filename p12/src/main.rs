ff8a189424ace4bdd099b9aa349524b2a5ce6538
Gerard <gerard@grdw.nl>
2024-12-12 08:51:15 +0100

    Green light for first example

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,10 +1,18 @@
 use std::fs;
+use std::collections::{HashMap, HashSet};
 
 const TRANSLATIONS: [(char, isize, isize); 4] = [
     ('T', -2, 0),  // TOP
     ('L', 0, -2),  // LEFT
     ('R', 0, 2),   // RIGHT
     ('B', 2, 0),   // BOTTOM
+];
+
+const FENCE_TRANSLATIONS: [(isize, isize); 4] = [
+    (-1, 0),  // TOP
+    (0, -1),  // LEFT
+    (0, 1),   // RIGHT
+    (1, 0),   // BOTTOM
 ];
 
 type RawGarden = Vec<Vec<Spot>>;
@@ -76,6 +84,19 @@ impl Garden {
             _ => ' '
         }
     }
+
+    fn debug(&self) {
+        for y in 0..self.ylen {
+            for x in 0..self.xlen {
+                match self.get(y, x) {
+                    Spot::Empty => print!(" "),
+                    Spot::Fence => print!("F"),
+                    Spot::Patch(c) => print!("{}", c)
+                }
+            }
+            println!("");
+        }
+    }
 }
 
 fn parse(input: &'static str) -> Garden {
@@ -87,11 +108,13 @@ fn parse(input: &'static str) -> Garden {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut patch = parse("input");
+    fence_off(&mut patch);
+    println!("p1 {}", total_fencing_cost(&patch));
 }
 
 fn fence_off(garden: &mut Garden) {
-    let mut fence_points = vec![];
+    let mut fence_points = HashSet::new();
 
     for y in 0..garden.ylen {
         for x in 0..garden.xlen {
@@ -105,15 +128,16 @@ fn fence_off(garden: &mut Garden) {
                 let (dy, dx) = (ty + y, tx + x);
 
                 let bc = garden.name(dy, dx);
+
                 if ac == bc { continue }
 
-                match dir {
-                    'T' => fence_points.push((y + 1, x)),
-                    'B' => fence_points.push((y - 1, x)),
-                    'L' => fence_points.push((y, x - 1)),
-                    'R' => fence_points.push((y, x + 1)),
+                let _ = match dir {
+                    'T' => fence_points.insert((y - 1, x)),
+                    'B' => fence_points.insert((y + 1, x)),
+                    'L' => fence_points.insert((y, x - 1)),
+                    'R' => fence_points.insert((y, x + 1)),
                     _ => panic!("Invalid direction"),
-                }
+                };
             }
         }
     }
@@ -121,20 +145,57 @@ fn fence_off(garden: &mut Garden) {
     for (fy, fx) in fence_points {
         garden.set(fy, fx, Spot::Fence);
     }
+
+    garden.debug()
 }
 
 fn total_fencing_cost(garden: &Garden) -> usize {
-    0
+    let mut total_area = HashMap::new();
+    let mut total_fencing = HashMap::new();
+
+    for y in 0..garden.ylen {
+        for x in 0..garden.xlen {
+            let name = garden.name(y, x);
+            if name == ' ' {
+                continue
+            }
+
+            total_area
+                .entry(name)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+
+            for (ty, tx) in &FENCE_TRANSLATIONS {
+                let (dy, dx) = (y + ty, x + tx);
+                if let Spot::Fence = garden.get(dy, dx) {
+                    total_fencing
+                        .entry(name)
+                        .and_modify(|count| *count += 1)
+                        .or_insert(1);
+                }
+            }
+        }
+    }
+
+    total_area
+        .iter()
+        .map(|(key, area)| area * total_fencing[key])
+        .sum()
 }
 
 #[test]
-fn test_fencing() {
+fn test_fencing_1() {
     let mut patch = parse("1");
     fence_off(&mut patch);
     assert_eq!(total_fencing_cost(&patch), 140);
+}
+
+#[test]
+fn test_fencing_2() {
     let mut patch = parse("2");
     fence_off(&mut patch);
     assert_eq!(total_fencing_cost(&patch), 772);
+
     let mut patch = parse("3");
     fence_off(&mut patch);
     assert_eq!(total_fencing_cost(&patch), 1930);
