8175eea225e8de7a26e100bfc3d0d61f71e0c044
grdw <gerard@grdw.nl>
2024-12-14 10:46:21 +0100

    Resolve part 1

--- p14/src/main.rs
+++ p14/src/main.rs
@@ -13,8 +13,8 @@ struct Robot {
 }
 
 fn main() {
-    let robots = parse("input");
-    println!("p1 {}", robot_positions(&robots, WIDTH, HEIGHT));
+    let mut robots = parse("input");
+    println!("p1 {}", robot_positions(&mut robots, WIDTH, HEIGHT));
 }
 
 fn parse(input: &'static str) -> Vec<Robot> {
@@ -37,14 +37,58 @@ fn parse_point(input: &str) -> (i16, i16) {
     (x, y)
 }
 
-fn robot_positions(robots: &Vec<Robot>, w: i16, h: i16) -> usize {
-    println!("{:?}", robots);
-    0
+fn robot_positions(robots: &mut Vec<Robot>, w: i16, h: i16) -> usize {
+    for _ in 0..TIME {
+        for robot in robots.iter_mut() {
+            robot.x += robot.vx;
+            robot.y += robot.vy;
+        }
+    }
+
+    for robot in robots.iter_mut() {
+        let nx = robot.x.rem_euclid(w);
+        let ny = robot.y.rem_euclid(h);
+        robot.x = nx;
+        robot.y = ny;
+    }
+
+    let hh = h / 2;
+    let wh = w / 2;
+    let mut quadrants = vec![0; 4];
+
+    for y in 0..h {
+        for x in 0..w {
+            let c = robots
+                .iter()
+                .filter(&&|r: &&Robot| r.y == y && r.x == x)
+                .count();
+
+            if c == 0 {
+                continue
+            }
+
+            let q = if y < hh && x < wh {
+                0
+            } else if y < hh && x > wh {
+                1
+            } else if y > hh && x < wh {
+                2
+            } else if y > hh && x > wh {
+                3
+            } else {
+                continue
+            };
+
+            quadrants[q] += c;
+        }
+    }
+
+    quadrants.iter().product()
 }
 
 #[test]
 fn test_robot_positions() {
-    let robots = parse("1");
-    assert_eq!(robot_positions(&robots, 11, 7), 12);
+    let mut robots = parse("1");
+    assert_eq!(robot_positions(&mut robots, 11, 7), 12);
 
 }
