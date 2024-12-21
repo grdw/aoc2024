eb7ca33957e32b47bf8fcbc56e840269c9338425
grdw <gerard@grdw.nl>
2024-12-21 13:09:28 +0100

    Resolve part 1

--- p21/src/main.rs
+++ p21/src/main.rs
@@ -4,8 +4,10 @@ const NUMERIC: &'static str = "789456123 0A";
 const DIRECTIONAL: &'static str = " ^A<v>";
 const SIZE: isize = 3;
 
+type Point = (isize, isize);
+
 fn main() {
-    println!("Hello, world!");
+    println!("p1 {}", shortest_inputs("input"));
 }
 
 fn shortest_inputs(input: &'static str) -> usize {
@@ -13,14 +15,8 @@ fn shortest_inputs(input: &'static str) -> usize {
 
     for line in fs::read_to_string(input).unwrap().lines() {
         let mut line = line.to_string();
-        let mut dir = to_directional(NUMERIC, &line);
-        println!("{:?}", dir);
-        dir = to_directional(DIRECTIONAL, &dir);
-        println!("{:?}", dir);
-        dir = to_directional(DIRECTIONAL, &dir);
-        println!("{:?}", dir);
+        let dir = to_my_input(&line);
 
-        println!("{} {}", line, dir.len());
         line.retain(|x| x.is_numeric());
         let t = line.parse::<usize>().unwrap();
 
@@ -34,28 +30,71 @@ fn test_shortest_inputs() {
     assert_eq!(shortest_inputs("1"), 126384);
 }
 
+// This is the Manhattan distance
+fn manhattan_dist(s: &Point, e: &Point) -> usize {
+    let dx = if s.0 < e.0 { e.0 - s.0 } else { s.0 - e.0 };
+    let dy = if s.1 < e.1 { e.1 - s.1 } else { s.1 - e.1 };
+    (dx + dy) as usize
+}
+
+fn to_my_input(numbers: &String) -> String {
+    let mut dir = to_directional(NUMERIC, numbers);
+    dir = to_directional(DIRECTIONAL, &dir);
+    dir = to_directional(DIRECTIONAL, &dir);
+    dir
+}
+
+#[test]
+fn test_shortest_route_basic() {
+    let input = "34".to_string();
+    let dir = to_directional(NUMERIC, &input);
+    assert_eq!(dir, String::from("^A<<^A"));
+    let dir = to_directional(DIRECTIONAL, &dir);
+    assert_eq!(dir, String::from("<A>Av<<AA>^A>A"));
+}
+
 fn to_directional(keypad: &'static str, numbers: &String) -> String {
     let mut directions = String::new();
     let mut start = pos(keypad, 'A');
+    let gap = pos(keypad, ' ');
 
     for cs in numbers.chars() {
         let end = pos(keypad, cs);
         let (y, x) = (start.0 - end.0, start.1 - end.1);
 
-        let ns = if y > 0 { "^" } else { "v" };
-        let ew = if x > 0 { "<" } else { ">" };
+        let ns = if y > 0 { b'^' } else { b'v' };
+        let ew = if x > 0 { b'<' } else { b'>' };
 
         let ya = y.abs() as usize;
         let xa = x.abs() as usize;
 
-        let d1 = (end.0, start.1);
+        let py = String::from_utf8(vec![ns; ya]).unwrap();
+        let px = String::from_utf8(vec![ew; xa]).unwrap();
 
-        if keypad.chars().nth(to_i(d1)) == Some(' ') {
-            directions.push_str(&ew.repeat(xa));
-            directions.push_str(&ns.repeat(ya));
+        let d1 = (start.0, end.1);
+        let d2 = (end.0, start.1);
+
+        if d1 == gap {
+            directions.push_str(&py);
+            directions.push_str(&px);
+        } else if d2 == gap {
+            directions.push_str(&px);
+            directions.push_str(&py);
         } else {
-            directions.push_str(&ns.repeat(ya));
-            directions.push_str(&ew.repeat(xa));
+            let dir_y = pos(DIRECTIONAL, ns as char);
+            let dir_x = pos(DIRECTIONAL, ew as char);
+            let dir_a = pos(DIRECTIONAL, 'A');
+
+            let my = manhattan_dist(&dir_y, &dir_a);
+            let mx = manhattan_dist(&dir_x, &dir_a);
+
+            if my > mx {
+                directions.push_str(&py);
+                directions.push_str(&px);
+            } else {
+                directions.push_str(&px);
+                directions.push_str(&py);
+            }
         }
 
         directions.push('A');
@@ -64,10 +103,6 @@ fn to_directional(keypad: &'static str, numbers: &String) -> String {
     }
 
     directions
-}
-
-fn to_i(pos: (isize, isize)) -> usize {
-    ((pos.0 * SIZE) + pos.1) as usize
 }
 
 fn pos(keypad: &'static str, lookup: char) -> (isize, isize) {
@@ -95,12 +130,15 @@ fn test_to_directional() {
 
     assert_eq!(
         to_directional(NUMERIC, &input),
-        String::from("<A^A^^>AvvvA")
+        String::from("<A^A>^^AvvvA")
     );
 
     assert_eq!(
-        to_directional(DIRECTIONAL, &to_directional(NUMERIC, &input)),
-        String::from("v<<A>>^A<A>A<AAv>A^Av<AAA^>A")
+        to_directional(
+            DIRECTIONAL,
+            &to_directional(NUMERIC, &input)
+        ).len(),
+        28
     );
 
     assert_eq!(
